3b2a3ee0f83a0ed5d53e750814a91372031fca5c
Dipsaint1 <ayodejioladapo96@gmail.com>
2022-06-11 22:40:48 +0100

    Added form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Movies from './components/movies';
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 class App extends Component {
   render() { 
     return (
-      <main className='container'>
-        <Movies/>
-      </main>
+      <>
+        <Header />
+        <Form/>
+      </>
     );
   }
 }
